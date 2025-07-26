0a692b6d412174e3523fd014dac3f16b28c80cdb
Twebubble <2366675132@qq.com>
2025-07-26 22:08:18 +0800

    musicure项目后端初版

---
+++ src/main/java/io/github/Twebubble/musicure/MusicureApplication.java
@@ -0,0 +1,13 @@
+package io.github.Twebubble.musicure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicureApplication.class, args);
+    }
+
+}
