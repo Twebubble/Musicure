aa02f9243b1fe74afda3a5db8b7dfecb8efca378
Twebubble <2366675132@qq.com>
2025-08-19 21:02:23 +0800

    添加了测试环境专用的文件操作工具类TestFileUtil.java和MyBatis-Plus提供的字段自动填充处理器MyMetaObjectHandler.java（用于@TableField）。

---
+++ src/main/java/io/github/Twebubble/musicure/handler/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package io.github.Twebubble.musicure.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+//MyBatis-Plus 提供的字段自动填充处理器，例如如下：
+//@TableField(fill = FieldFill.INSERT)
+//@TableField(fill = FieldFill.INSERT_UPDATE)
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
