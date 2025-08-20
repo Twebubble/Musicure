b3426259c7b89326c7f385a179a927aee6d048df
Twebubble <2366675132@qq.com>
2025-08-20 21:42:42 +0800

    修改了eslint的格式检查属性。在package.json增加了element-plus和icon的依赖。关闭了TypeStript的变量类型指定检查的属性（tsconfig.json）。编写了request.ts和index.ts，配置与后端连接的axios网络请求核心封装模块，集中与后端交互的API请求。

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,19 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 0,
+    "@typescript-eslint/no-unused-vars": ["off"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    'no-undef': 0,
+    // // 核心规则：设置tab缩进为 2 格
+    // "indent": ["error", 2, {
+    //   "SwitchCase": 1, // switch 语句的 case 缩进特殊处理（可选）
+    //   "ignoredNodes": ["TemplateLiteral > *"] // 忽略模板字符串内的缩进检测（Vue 模板中可能需要）
+    // }],
+    // "vue/script-indent": ["error", 2, {
+    //   "baseIndent": 1, // 脚本块（<script>）的基础缩进
+    //   "switchCase": 1
+    // }]
   }
 }
