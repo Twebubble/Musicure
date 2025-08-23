7d95ab8e0256ed2fc898d2123b6817279d517416
Twebubble <2366675132@qq.com>
2025-08-23 22:50:42 +0800

    完成了Search视图的编写。在tsconfig.json中加入了对.d.ts文件的识别。

---
+++ src/components/layouts/MusicureNav.vue
@@ -0,0 +1,87 @@
+<template>
+    <ul class="musicure-nav">
+        <li v-for="(item, index) in styleList" :key="index" :class="{ active: item.name == activeName }"
+            @click="handleChangeView(item)">
+            {{ item.name }}
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, getCurrentInstance, PropType } from "vue";
+
+// 定义样式项接口【新增：解决item报错问题】
+interface NavItem {
+    name: string;
+    value?: string; // 根据 Search.vue 的使用，还有 value 属性
+    [key: string]: any; // 允许其他属性
+}
+
+export default defineComponent({
+    props: {
+        // styleList: Array,
+        // activeName: String,
+        styleList: {
+            ttype: Array as PropType<NavItem[]>,
+            default: () => [] as NavItem[]
+        },
+        activeName: {
+            type: String,
+            default: ""
+        }
+    },
+    emits: ["click"],
+    // methods: {
+    //     handleClick(item) {
+    //         this.$emit("click", item); // 触发点击事件
+    //     }
+    // },
+    setup() {
+        const { proxy } = getCurrentInstance();
+
+        function handleChangeView(val) {
+            proxy.$emit("click", val);
+        }
+        return {
+            handleChangeView,
+        };
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/var.scss";
+
+.musicure-nav {
+    width: 100%;
+
+    li {
+        line-height: 2rem;
+        font-size: 1rem;
+        color: $color-grey;
+        border-bottom: none;
+        cursor: pointer;
+    }
+
+    li.active {
+        color: $color-black;
+        font-weight: 600;
+    }
+}
+
+@media screen and (min-width: $sm) {
+    .yin-nav {
+        li {
+            margin: 0.5rem 1rem;
+        }
+    }
+}
+
+@media screen and (max-width: $sm) {
+    .yin-nav {
+        li {
+            margin: 0.3rem 0.4rem;
+        }
+    }
+}
+</style>
---
+++ src/views/search/Search.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="search">
+        <musicure-nav :styleList="searchNavList" :activeName="activeName" @click="handleChangeView"></musicure-nav>
+        <component class="search-list" :is="currentView"></component>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MusicureNav from "@/components/layouts/MusicureNav.vue";
+import SearchSong from "./SearchSong.vue";
+import SearchSongList from "./SearchSongList.vue";
+
+export default defineComponent({
+    components: {
+        MusicureNav,
+        SearchSong,
+        SearchSongList,
+    },
+    data() {
+        return {
+            searchNavList: [
+                {
+                    name: "歌曲",
+                    value: "SearchSong",
+                },
+                {
+                    name: "歌单",
+                    value: "SearchSongList",
+                },
+            ],
+            activeName: "歌曲",
+            currentView: "SearchSong",
+        };
+    },
+    methods: {
+        handleChangeView(item) {
+            this.activeName = item.name;
+            this.currentView = item.value;
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/var.scss";
+@import "@/assets/css/global.scss";
+
+.search {
+    margin: auto;
+    width: 900px;
+
+    .search-list {
+        min-height: 480px;
+    }
+}
+</style>
